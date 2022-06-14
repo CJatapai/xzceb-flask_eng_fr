97497291b9d8553ceca0a890fe87c8885d82d1a5
CJatapai <92006365+CJatapai@users.noreply.github.com>
2022-06-14 15:02:47 +0700

    Update translator.py

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,4 +1,4 @@
-import json
+'''Translator'''
 import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
@@ -13,18 +13,16 @@ authenticator = IAMAuthenticator(apikey)
 language_translator = LanguageTranslatorV3(
     version='2018-05-01',
     authenticator=authenticator)
-
 language_translator.set_service_url(url)
 
 def English_to_French(english_text):
     """English to French"""
     frenchtrans = language_translator.translate(text=english_text, model_id='en-fr').get_result()
     return frenchtrans["translations"][0]["translation"]
-    
-
 
 def French_to_English(french_text):
     """French to English"""
     englishtrans = language_translator.translate(text=french_text, model_id='fr-en').get_result()
     return englishtrans["translations"][0]["translation"]
+
     
